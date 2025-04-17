c1a7860b934c3305959159e3ec78b384a91e61a6
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-04-16 21:43:27 -0300

    criando o primeiro component angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstComponent } from './my-first/my-first.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MyFirstComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'my-first-component',
+  template: `
+  <p>My first component with Angular 2!</p>
+  `
+})
+export class MyFirstComponent { }
