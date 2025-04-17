209d544afc0ae8b4e1633c416f9d901bc10b66ce
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2025-04-16 21:49:23 -0300

    segundo componenete, esse gerado pelo angular CLI

---
+++ src/app/my-second/my-second.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-second',
+  imports: [],
+  templateUrl: './my-second.component.html',
+  styleUrl: './my-second.component.css'
+})
+export class MySecondComponent {
+
+}
